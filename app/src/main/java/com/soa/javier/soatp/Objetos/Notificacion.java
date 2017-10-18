dcc97b84cc92412c105c858b0d8cd3ed3ebdd3b1
mcortex <cortezmartin1983@gmail.com>
2017-10-18 19:52:45 -0300

    Cambios Fer

--- app/src/main/java/com/soa/javier/soatp/Objetos/Notificacion.java
+++ app/src/main/java/com/soa/javier/soatp/Objetos/Notificacion.java
@@ -1,21 +1,35 @@
 package com.soa.javier.soatp.Objetos;
 import android.text.format.DateUtils;
 
+import android.util.TimeUtils;
+
 import java.util.Date;
 
 /**
  * Created by ferbroqua on 17/10/2017.
  */
 
+
 public class Notificacion {
+    public static final int URG_0EMERGENCIA = 0;
+    public static final int URG_1ATENCION = 10;
+    public static final int URG_2AVISO = 20;
+    public static final int URG_3NOIMPORTANTE = 30;
+    public static final int EST_0MARCADO = 0;
+    public static final int EST_1NOLEIDO = 10;
+    public static final int EST_2LEIDO = 20;
+    public static final int EST_3OCULTO = 30;
+
     private Date fecha;
-    private String estado;
+    private int estado;   // EST_*
     private String mensaje;
-    private int urgencia;   // 0= EMERGENCIA    1= ATENCION 2= AVISO    3= POCO IMPORTANTE
+    private int urgencia; // URG_*
 
     public Notificacion (String mensaje, int urgencia) {
+        //DateFormat date = new SimpleDateFormat("MMM dd yyyy, h:mm");
+        //String dateFormatted = date.format(Calendar.getInstance().getTime());
         this.fecha = new Date();
-        this.estado = "NO LEIDO";
+        this.estado = EST_1NOLEIDO;
         this.mensaje = mensaje;
         this.urgencia = urgencia;
     }
